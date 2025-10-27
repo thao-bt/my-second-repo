a4bd35e81dd63adfc0900e8db2074540d8c9844e
thao-bt <buithanhthao284@gmail.com>
2025-10-27 12:17:04 +0700

    Update System.cpp

--- System.cpp
+++ System.cpp
@@ -6,8 +6,22 @@ using namespace std;
 
 const int MAX = 100;
 
-void tachNgay(const string &ngay, int &d, int &m, int &y);
-bool truocNgay(const string &ngay, const string &mocSoSanh);
+void tachNgay(const string &ngay, int &d, int &m, int &y) {
+    if (ngay.size() < 10) { d = m = y = 0; return; }
+    d = stoi(ngay.substr(0, 2));
+    m = stoi(ngay.substr(3, 2));
+    y = stoi(ngay.substr(6, 4));
+}
+
+bool truocNgay(const string &ngay1, const string &ngay2) {
+    int d1, m1, y1, d2, m2, y2;
+    tachNgay(ngay1, d1, m1, y1);
+    tachNgay(ngay2, d2, m2, y2);
+    if (y1 != y2) return y1 < y2;
+    if (m1 != m2) return m1 < m2;
+    return d1 < d2;
+}
+
 
 class System {
 private:
